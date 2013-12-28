1309f2aaf9faada6457f98ef51edf9c2c94b969e
Alan Shaw <alan@freestyle-developments.co.uk>
2013-12-28 00:05:49 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var Tokenizer = require("tokenizer")
+
+module.exports = function (cb) {
+  var t = new Tokenizer(cb)
+
+  t.addRule(/^::([_A-Za-z][A-Za-z0-9_\-]*)$/, "namespace keyword")
+  t.addRule(/^:([_A-Za-z][A-Za-z0-9_\-]*)$/, "keyword")
+  t.addRule(/^([_A-Za-z][A-Za-z0-9_\-]*)$/, "symbol")
+
+  t.addRule(/^;;[^\n]*$/, "line comment")
+
+  t.addRule(/^\($/, "open paren")
+  t.addRule(/^\)$/, "close paren")
+  t.addRule(/^\[$/, "open square")
+  t.addRule(/^\]$/, "close square")
+  t.addRule(/^{$/, "open curly")
+  t.addRule(/^}$/, "close curly")
+
+  t.addRule(/^(#|')$/, "operator")
+
+  t.addRule(/^[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?$/, "number")
+  t.addRule(Tokenizer.whitespace)
+
+  return t
+}
